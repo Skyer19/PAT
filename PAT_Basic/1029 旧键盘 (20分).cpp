f6fe7d1b61c62aec9bdbb952a7b6bb99187c999d
mengyu666 <rmy200929@163.com>
2020-07-27 00:00:44 +0800

    2020/7/27

--- PAT_Basic/1029 旧键盘 (20分).cpp
+++ PAT_Basic/1029 旧键盘 (20分).cpp
@@ -1,55 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	string s1,s2,s3;
+int main(){
+	string s1, s2, ans;
 	cin>>s1>>s2;
-	int point1 =0, point2 = 0;
-	int judge[s1.length()];
-	while(point2<=s2.length()) {
-		if(s1[point1]!=s2[point2]) {
-			point1++;
-		} else {
-			judge[point1] = 1;
-			point1++;
-			point2++;
+	for(int i = 0; i < s1.length(); i++){
+		if(s2.find(s1[i]) == string::npos&&ans.find(toupper(s1[i])) == string::npos){
+			ans+=toupper(s1[i]);
 		}
+		cout<<ans<<endl;
+		return 0;
 	}
-	for(int i = 0; i < s1.length(); i++) {
-		int flag = 0;
-		if(judge[i]!=1) {
-			for(int j =0; j < s3.length(); j++) {
-				if(s1[i]==s3[j]) {
-					break;
-					flag =1;
-				}
-				if(s1[i]>='a'&&s1[i]<='z'&&(char)(s1[i]-('a'-'A'))==s3[j]) {
-					break;
-					flag = 1;
-				}
-				if(s1[i]>='A'&&s1[i]<='Z'&&s1[i]==s3[j]) {
-					break;
-					flag =1;
-				}
-			}
-			if(flag!=1) {
-				if(s1[i]>='a'&s1[i]<='z') {
-					s3+=(char)(s1[i]-('a'-'A'));
-				} else {
-					s3+=s1[i];
-				}
-			}
-			cout<<"######################"<<endl;
-			for(int i = 0; i< s3.length(); i++) {
-				cout<<s3[i];
-			
-			}
-				cout<<"######################"<<endl;
-		}
-	}
-	for(int i = 0; i< s3.length(); i++) {
-		cout<<s3[i];
-	}
-
-	return 0;
 }
