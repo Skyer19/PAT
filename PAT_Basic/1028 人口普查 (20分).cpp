c6948162b8816de31db8d05688872b7270316ccb
mengyu666 <rmy200929@163.com>
2020-07-26 23:48:50 +0800

    2020/7/26

--- PAT_Basic/1028 人口普查 (20分).cpp
+++ PAT_Basic/1028 人口普查 (20分).cpp
@@ -1,50 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct people {
-	string name;
-	int year;
-	int month;
-	int day;
-	bool judge;
-};
-bool cmp(people c, people d) {
-	if(c.year<d.year) {
-		return c.year<d.year;
-	} else if (c.year==d.year&c.month<d.month) {
-		return c.month<d.month;
-	} else if(c.year==d.year&c.month==d.month&c.day<d.day) {
-		return c.day<d.day;
+int main(){
+	int N, cut = 0;
+	cin>>N;
+	string name, maxname, minname, birth, maxbirth = "1814/09/06", minbirth = "2014/09/06";
+	
+	while(N--){
+		cin>>name>>birth;
+		if(birth<="2014/09/06"&&birth>="1814/09/06"){
+			cut++;
+			if(birth>=maxbirth){
+				 maxname = name;
+				 maxbirth = birth;
+			}
+			if(birth<=minbirth){
+				minname = name;
+				minbirth = birth;
+			}
+		} 
 	}
-}
-int t;
-int main() {
-	cin>>t;
-	people p [t];
-	for(int i = 0; i < t; i++) {
-		scanf("%s",&p[i].name);
-		cin>>p[i].year>>p[i].month>>p[i].day;
-		if((p[i].year>=2014&&p[i].month>=9&&p[i].day>=6)||p[i].year>2015||(p[i].year>=1814&&p[i].month<=9&&p[i].day<=6)||p[i].year<1813) {
-			p[i].judge = true;
-		}
-	}
-	sort(p,p+t,cmp);
-	int sum;
-	for(int i = 0; i < t; i++) {
-		if(!p[i].judge) {
-			sum++;
-		}
-	}
-		for(int i = t-1; i >= 0; i--) {
-		if(!p[i].judge) {
-			cout<<p[i].name<<endl;
-			break;
-		}
-	}
-		for(int i = 0; i < t; i++) {
-		if(!p[i].judge) {
-			cout<<p[i].name<<endl;
-			break;
-		}
-	}
+		cout<<cut;
+		if(cut!=0)cout<<" "<<minname<<" "<<maxname<<endl;
+		return 0;
+	
 }
