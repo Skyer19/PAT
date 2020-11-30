b030d7b13872d17af2bb3515ac0327c76f6679b6
mengyu666 <rmy200929@163.com>
2020-11-30 20:52:06 +0800

    2020/11/30

--- PAT_Basic/1094 谷歌的招聘 (20分).cpp
+++ PAT_Basic/1094 谷歌的招聘 (20分).cpp
@@ -1,2 +1,31 @@
 # include<bits/stdc++.h>
 using namespace std;
+
+// 觉得像是滑动窗口
+bool isPrime(int n) {
+	if(n==0||n==1) return false;
+	for(int i = 2; i * i <= n; i++) {
+		if(n%i==0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int l, k;
+	string n;
+	cin>>l>>k>>n;
+
+	for(int i = 0; i <= l - k; i ++) {
+		string temp = n.substr(i , k);
+		int num = stoi(temp);
+		if(isPrime(num)) {
+			cout<<temp;  //注意不是  cout<<num; 
+			return 0;
+		}
+	}
+    cout << "404\n";
+    return 0;
+
+}
